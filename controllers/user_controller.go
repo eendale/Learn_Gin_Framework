f493333bac9ebf295243fcc3cfcf83b6357e6721
Endale2 <endale406@gmail.com>
2025-11-15 13:05:28 +0300

    updated

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"context"
+	"net/http"
 	"time"
 
 	"github.com/Endale2/Learn_Gin_Framework/db"
@@ -12,18 +13,17 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-
-
-func  getCollection()*mongo.Collection{
+func getCollection() *mongo.Collection {
 	return db.DB.Collection("users")
 }
 
-// Create a new user
 func CreateUser(c *gin.Context) {
-	userCollection:=getCollection()
-	var user models.User
+	userCollection := getCollection()
+
+	user := models.User{}
+
 	if err := c.ShouldBindJSON(&user); err != nil {
-		c.JSON(400, gin.H{"error": "Error binding JSON"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON"})
 		return
 	}
 
@@ -31,103 +31,41 @@ func CreateUser(c *gin.Context) {
 	defer cancel()
 
 	result, err := userCollection.InsertOne(ctx, user)
+
 	if err != nil {
-		c.JSON(500, gin.H{"error": "Failed to create user"})
+		c.JSON(500, gin.H{"error": "Failed to  Create  user!"})
 		return
 	}
 
 	user.ID = result.InsertedID.(primitive.ObjectID)
+
 	c.JSON(201, user)
+
 }
 
-// Get all users
+//get all users
+
 func GetUsers(c *gin.Context) {
-	userCollection:=getCollection()
+	userCollection := getCollection()
+
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	cursor, err := userCollection.Find(ctx, bson.M{})
+
 	if err != nil {
-		c.JSON(500, gin.H{"error": "Failed to fetch users"})
+		c.JSON(500, gin.H{"errors": err})
 		return
 	}
+
 	defer cursor.Close(ctx)
 
 	var users []models.User
-	if err = cursor.All(ctx, &users); err != nil {
-		c.JSON(500, gin.H{"error": "Failed to parse users"})
+	err = cursor.All(ctx, &users)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err})
 		return
 	}
-    
+
 	c.JSON(200, users)
-}
-
-// Delete user
-func DeleteUser(c *gin.Context) {
-	userCollection:=getCollection()
-	id := c.Param("id")
-	objectID, err := primitive.ObjectIDFromHex(id)
-	if err != nil {
-		c.JSON(400, gin.H{"error": "Invalid ID"})
-		return
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	result, err := userCollection.DeleteOne(ctx, bson.M{"_id": objectID})
-	if err != nil {
-		c.JSON(500, gin.H{"error": "Failed to delete user"})
-		return
-	}
-
-	if result.DeletedCount == 0 {
-		c.JSON(404, gin.H{"error": "User not found"})
-		return
-	}
-
-	c.JSON(200, gin.H{"message": "User deleted successfully"})
-}
-
-// Update an existing user
-func UpdateUser(c *gin.Context) {
-	userCollection := getCollection()
-	id := c.Param("id")
-
-	objectID, err := primitive.ObjectIDFromHex(id)
-	if err != nil {
-		c.JSON(400, gin.H{"error": "Invalid ID"})
-		return
-	}
-
-	var user models.User
-	if err := c.ShouldBindJSON(&user); err != nil {
-		c.JSON(400, gin.H{"error": "Error binding JSON"})
-		return
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	update := bson.M{
-		"$set": bson.M{
-			"first_name": user.FirstName,
-			"last_name":  user.LastName,
-			"email":      user.Email,
-		},
-	}
-
-	result, err := userCollection.UpdateOne(ctx, bson.M{"_id": objectID}, update)
-	if err != nil {
-		c.JSON(500, gin.H{"error": "Failed to update user"})
-		return
-	}
-
-	if result.MatchedCount == 0 {
-		c.JSON(404, gin.H{"error": "User not found"})
-		return
-	}
-
-	user.ID = objectID
-	c.JSON(200, user)
 }
--- models/user.go
+++ models/user.go
@@ -3,9 +3,10 @@ package models
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-type User struct {
-	ID   primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	FirstName string `bson:"first_name" json:"first_name"`
-	LastName string `bson:"last_name" json:"last_name"`
-	Email string `bson:"email" json:"email"`
+type  User struct {
+
+	ID  primitive.ObjectID `bson:"_id" json:"id"`
+	FirstName  string       `bson:"first_name" json:"first_name"`
+	LastName  string         `bson:"last_name" json:"last_name"`
+	Email       string       `bson:"email" json:"email"`
 }
