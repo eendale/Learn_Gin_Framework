b643ca7f6d14d8fb53128b09a0d847d6315058bb
Endale2 <endale406@gmail.com>
2025-11-15 14:47:32 +0300

    updated

--- models/user.go
+++ models/user.go
@@ -5,7 +5,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type  User struct {
 
-	ID  primitive.ObjectID `bson:"_id" json:"id"`
+	ID  primitive.ObjectID `bson:"_id,omitempty" json:"id"`
 	FirstName  string       `bson:"first_name" json:"first_name"`
 	LastName  string         `bson:"last_name" json:"last_name"`
 	Email       string       `bson:"email" json:"email"`
