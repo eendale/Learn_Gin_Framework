f8042c967e0f3b1e36de76df165e021c9d89fec1
Endale2 <endale406@gmail.com>
2025-11-23 15:28:46 +0300

    updated

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/Endale2/Learn_Gin_Framework/db"
@@ -96,6 +97,128 @@ func GetUser(c *gin.Context) {
 		return
 	}
 
-	
 	c.JSON(200, user)
+}
+
+func UpdateUser(c *gin.Context) {
+
+	userColl := getCollection()
+	var user models.User
+
+	id := c.Param("id")
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		c.JSON(404, gin.H{"error": "Invalid ID"})
+		return
+	}
+
+	err = c.ShouldBindJSON(&user)
+	if err != nil {
+		c.JSON(404, gin.H{"error": "Error Binding User"})
+		return
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+   updateFields:=bson.M{}
+
+   if user.FirstName!=""{
+	if len(user.FirstName)<2{
+		c.JSON(400, gin.H{"error":"First Name must be at least 2 characters!"})
+		return
+	}
+	updateFields["first_name"] =user.FirstName
+   }
+
+   if user.LastName!=""{
+	if len(user.LastName)<2{
+		c.JSON(400, gin.H{"error":"Last Name must be at least 2 characters"})
+		return
+	}
+
+	updateFields["last_name"]=user.LastName
+   }
+
+   if user.Email!=""{
+	if !strings.Contains(user.Email, "@"){
+		c.JSON(400, gin.H{"error":"Invalid Email!"})
+		return
+	}
+
+	updateFields["email"]=user.Email
+   }
+	update := bson.M{"$set":updateFields}
+	result, err := userColl.UpdateOne(ctx, bson.M{"_id": objID}, update)
+
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Unable to Fetch User"})
+		return
+	}
+
+	if result.MatchedCount == 0 {
+		c.JSON(404, gin.H{"error": "User Not Found!"})
+		return
+	}
+
+	c.JSON(200, gin.H{"message": "User Updated Successfully!"})
+}
+
+func DeleteUser(c *gin.Context) {
+	userColl := getCollection()
+	objID, err := primitive.ObjectIDFromHex(c.Param("id"))
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid ID!"})
+		return
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	result, err := userColl.DeleteOne(ctx, bson.M{"_id": objID})
+
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Unable to Delete User!"})
+		return
+	}
+	if result.DeletedCount == 0 {
+		c.JSON(404, gin.H{"error": "User Not Found!"})
+		return
+	}
+
+	c.JSON(200, gin.H{"message": "Deleted Successfully!"})
+}
+
+func SearchUser(c *gin.Context) {
+	userColl := getCollection()
+
+	email := strings.TrimSpace(c.Query("email"))
+
+	if email == "" {
+		c.JSON(400, gin.H{"error": "Email Required!"})
+		return
+	}
+	var users []models.User
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	cursor, err := userColl.Find(ctx, bson.M{
+		"email": bson.M{
+			"$regex": ".*" + email, "$options": "i",
+		},
+	})
+
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Error Fetching users"})
+		return
+	}
+
+	defer cursor.Close(ctx)
+	err = cursor.All(ctx, &users)
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Error Decoding  Users!"})
+		return
+	}
+	if len(users) == 0 {
+		c.JSON(404, gin.H{"error": "No Users Found!"})
+		return
+	}
+
+	c.JSON(200, users)
 }
